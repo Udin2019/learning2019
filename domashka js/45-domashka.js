43de93becb22f7dd7683f8117495e81121cc5d86
Udin2019 <udin@seospin.ru>
2019-11-07 19:10:01 +0300

    Domashka - 4.6 ready

--- domashka js/45-domashka.js
+++ domashka js/45-domashka.js
@@ -1,9 +1,47 @@
-let arr1 = new Array(6)
-console.log (arr1.length)
-arr1[0] = 'torr'
-arr1[1] = 'loki'
-arr[2] = 4
-arr[3] = 49
-arr[4] = true
-arr[5] = false
-console.log (arr1.length)
+// 4.6 Domashka
+
+
+function myFunction() {
+    let x = document.getElementById("myInput").value;
+    document.getElementById("demo").innerHTML = "Владимир";
+  }
+
+// 4.5 domashka 
+
+// // let arr = new Array(5)
+
+
+// // arr[0] = 'string'
+// // arr[1] = 'Tor'
+// // arr[2] = 45
+// // arr[3] = 90
+// // arr[4] = false
+// // arr[5] = true
+// // console.log(arr)
+
+// let a3 = {
+//     3 : 'hello',
+//     'one' : 'hi',
+//     'testt' : 'vodoley',
+//     'ivan' : 'ivanov',
+//     };
+
+
+// for (let key in a3) {
+
+// console.log(`${key}`)  
+// }
+// //     for (let i = 0; i < key.length; i++) {
+// //         let keys = key[0]
+// //     }
+
+// //     console.log(keys)
+
+
+// // // function compareFun(x, y) {
+// // //     if( x > y ) return 1
+// // //     if( x == y ) return 0
+// // //     if( x < y ) return -1
+// // // }
+
+// // // key.sort(compareFun);
