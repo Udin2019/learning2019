a5637f7311d492b55036e2c27c9378e7cbe3635f
Udin2019 <udin@seospin.ru>
2019-11-10 20:33:40 +0300

    4.5 - Domashka

--- domashka js/45-domashka.js
+++ domashka js/45-domashka.js
@@ -1,12 +1,14 @@
-// 4.6 Domashka
+// // // 4.6 Domashka
 
 
-function myFunction() {
-    let x = document.getElementById("myInput").value;
-    document.getElementById("demo").innerHTML = "Владимир";
-  }
+// // function myFunction() {
+// //     let x = document.getElementById("myInput").value;
+// //     document.getElementById("demo").innerHTML = "Владимир";
+// //   }
 
-// 4.5 domashka 
+// // 4.5 domashka 
+
+// // 1 задание 
 
 // // let arr = new Array(5)
 
@@ -17,31 +19,74 @@ function myFunction() {
 // // arr[3] = 90
 // // arr[4] = false
 // // arr[5] = true
+
 // // console.log(arr)
 
-// let a3 = {
-//     3 : 'hello',
-//     'one' : 'hi',
-//     'testt' : 'vodoley',
-//     'ivan' : 'ivanov',
-//     };
+// // 2 задание 
 
 
-// for (let key in a3) {
+// // let a3 = {
+// //     3: 'hello',
+// //     'one': 'hi',
+// //     'testt': 'vodoley',
+// //     'ivan': 'ivanov',
+// // };
 
-// console.log(`${key}`)  
-// }
-// //     for (let i = 0; i < key.length; i++) {
-// //         let keys = key[0]
+// // for (let key in a3) {
+// //     if (key.length > 4) {
+// //         console.log(`${key}`)
 // //     }
+// // }
 
-// //     console.log(keys)
+// // // 3 задание
+
+let a7 = {     
+    key: "value", 
+  };
+
+function wtite_input () {
+  let quantity = document.getElementById("quantity").value;
+  document.getElementById("otpravit").onclick = write_input;
+}
+
+ 
+// button.addEventListener('click', function() {
+//   inputVal = document.querySelector('input').value;
+//   a7 = inputVal.set(',');
+//   console.log(a7);
+// });
+
+// // 4 задание 
+
+// const a = ['Tor', 'Lokki', 'Odin', 34, 'privet']
+
+// console.log(a.length)
+
+// // 5 задание 
+
+// let d = [0, 2, 5, -4, 6, 22, -9, -12, , 13, 78]
+
+//   let a1 = d.filter(function(i) {
+//     return i % 2 != 0 ;
+    
+//   }); 
+//   console.log(a1)
+
+//   let a2 = d.filter(function(i) {
+//     return i % 2 == 0 ;
+//   }); 
+//   console.log(a2)
+
+  // 6 задание
+
+// d = [0, 2, 5, -4, 6, 22, -9, -12, 13, 78]
+
+// let max = Math.max.apply(null, d);
+
+// console.log(max)
 
 
-// // // function compareFun(x, y) {
-// // //     if( x > y ) return 1
-// // //     if( x == y ) return 0
-// // //     if( x < y ) return -1
-// // // }
 
-// // // key.sort(compareFun);
+
+
+
