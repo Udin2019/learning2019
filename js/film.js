484b96c9538762b9000bb9464416f5dd8763c952
Udin2019 <udin@seospin.ru>
2019-11-19 20:45:59 +0300

    4.8 - Domashka

--- js/film.js
+++ js/film.js
@@ -1,25 +1,3 @@
-// const ganar_1 = 'фантастика'
-// const ganar_2 = 'боевик'
-// const ganar_3 = 'приключения'
-// const ganar_4 = 'фэнтези'
-// const ganar_5 = 'драма'
-// const ganar_6 = 'комедия'
-// const ganar_7 = 'мультфильм'
-
-// let start_film_1 = '10:00'
-// let start_film_2 = '12:00'
-// let start_film_3 = '14:00'
-// let start_film_4 = '16:00'
-
-// let name_film_1 = 'Человек паук'
-// let name_film_2 = 'Собачья жизнь 2'
-// let name_film_3 = 'История игрушек 4'
-// let name_film_4 = 'Люди в черном: Интернэшнл'
-
-// let ganar_film_1 = ganar_1 + ', ' + ganar_2 +', '+ ganar_3
-// let ganar_film_2 = ganar_4 + ', ' + ganar_5 +', '+ ganar_6
-// let ganar_film_3 = ganar_7 + ', ' + ganar_4 +', '+ ganar_6
-// let ganar_film_4 = ganar_1 + ', ' + ganar_2 +', '+ ganar_6
 
 
 films = [
@@ -28,7 +6,7 @@ films = [
         start: '12:00',
         ganar: [0, 1],
         hire: true,
-        new: true,
+        new: false,
         description: 'Hope we see you at the event!',
     },
     film2 = {
@@ -83,7 +61,7 @@ for (let i = 0; i < films.length; i++) {
         filmsNew.push(films[i])
     }
 }
-
+// может filmHire
 const film = {
     getName: function () {
         return this.name
@@ -94,10 +72,10 @@ const film = {
     getGanar: function () {
         const ganarsFilm = this.ganar
         let arrGanars = []
-        for (let i = 0; i < ganarsFilm; i++) {  
-            arrstrGanars.push(ganars[ganarsFilm[i]])
+        for (let i = 0; i < ganarsFilm.lenght; i++) {
+            arrGanars.push(ganars[ganarsFilm[i]])
         }
-        let atrGanars = arrGanars.join(', ')
+        let strGanars = arrGanars.join(', ')
         return strGanars
     }
 }
@@ -106,58 +84,20 @@ for (let i = 0; i < filmsHire.lenght; i++) {
     const filmName = film.getName.bind(filmsHire[i])()
     const filmStart = film.getStert.bind(filmsHire[i])()
     const filmGanar = film.getGanar.bind(filmsHire[i])()
-    let filmsHireHTML = docoment.getElementById('filmsHire')
+    let filmsHireHTML = document.getElementById('filmsHire')
 
-let filmHTML = <tr>
-    <td>
-        <input type="checkbox" class="block0${i}__checkbox" id="block03__checkbox1"></input>
-<label for="block03__checkbox1"> <i class="icon icon-chek" title="check"></i></label>    
-    </td>
-<td id="start_film_${1}">${filmStart}</td>
-<td id="name_film_${1}">${filmName}</td>
-<td id="ganar_film_${1}">${filmGanar}</td>
-</tr>
-let tr = document.createElement("tr")
-tr.innerHTML = filmHTML
-filmHire.appendChild()
+
+
+    let filmHTML = `
+
+        <td id="start_film_${1}">${filmStart}</td>
+        <td id="name_film_${1}">${filmName}</td>
+        <td id="ganar_film_${1}">${filmGanar}</td>`
+
+    let tr = document.createElement("tr")
+    tr.innerHTML = filmHTML
+    filmsHireHTML.appendChild(tr)
 }
 
 
 
-// let element_start_film_1 = document.getElementById('start_film_1')
-// let element_name_film_1 = document.getElementById('name_film_1')
-// let element_ganar_film_1 = document.getElementById('ganar_film_1')
-
-// let element_start_film_2 = document.getElementById('start_film_2')
-// let element_name_film_2 = document.getElementById('name_film_2')
-// let element_ganar_film_2 = document.getElementById('ganar_film_2')
-
-// let element_start_film_3 = document.getElementById('start_film_3')
-// let element_name_film_3 = document.getElementById('name_film_3')
-// let element_ganar_film_3 = document.getElementById('ganar_film_3')
-
-// let element_start_film_4 = document.getElementById('start_film_4')
-// let element_name_film_4 = document.getElementById('name_film_4')
-// let element_ganar_film_4 = document.getElementById('ganar_film_4')
-
-// element_start_film_1.innerHTML = start_film_1
-// element_name_film_1.innerHTML = name_film_1
-// element_ganar_film_1.innerHTML = ganar_film_1
-
-// element_start_film_2.innerHTML = start_film_2
-// element_name_film_2.innerHTML = name_film_2
-// element_ganar_film_2.innerHTML = ganar_film_2
-
-// element_start_film_3.innerHTML = start_film_3
-// element_name_film_3.innerHTML = name_film_3
-// element_ganar_film_3.innerHTML = ganar_film_3
-
-// element_start_film_4.innerHTML = start_film_4
-// element_name_film_4.innerHTML = name_film_4
-// element_ganar_film_4.innerHTML = ganar_film_4
-
-
-// document.getElementById('name_film_1')
-// document.getElementById('name_film_2')
-// document.getElementById('name_film_3')
-// document.getElementById('name_film_4')
